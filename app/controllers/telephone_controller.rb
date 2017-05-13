cd4675f692337bc27abdafe575f18b5bed09c62c
Sam Lerner <samlerner@sams-mbp.attlocal.net>
2017-05-13 13:28:37 -0400

    commit message

--- app/controllers/telephone_controller.rb
+++ app/controllers/telephone_controller.rb
@@ -50,15 +50,33 @@ class TelephoneController < ApplicationController
   def finish_survey
   end
 
+  def format_data
+    original_out = $stdout.clone
+    $stdout = File.new('data.csv', 'w')
+    Chain.all.each do |chain|
+      chain.passages.all.each do |passage|
+        print chain.identifier + ','
+        print passage.passage_id + ','
+        print passage.number.to_s + ','
+        print passage.text + ','
+        print passage.derivation.to_s + ','
+        print passage.time_spent.to_s + ','
+        puts ''
+      end
+    end
+    $stdout = original_out
+  end
+
   private
+
   def user_params
     { chain_id: params[:chain_id].to_i, name: params[:name].to_s, place: params[:place].to_i }
   end
 
   def survey_params
     { input: params[:input].to_s, number: params[:index].to_i,
-    place: params[:place].to_i, chain_id: params[:chain].to_s,
-    time: params[:time_spent].to_i, visited: params[:visited].to_s,
-    pid: params[:pid].to_s }
+      place: params[:place].to_i, chain_id: params[:chain].to_s,
+      time: params[:time_spent].to_i, visited: params[:visited].to_s,
+      pid: params[:pid].to_s }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/home', to: 'telephone#survey'
   get '/start', to: 'telephone#start_survey'
   get '/finish', to: 'telephone#finish_survey'
+  get '/format_data', to: 'telephone#format_data'
 end
