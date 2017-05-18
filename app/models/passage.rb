26f58a0ad7d945f0dad4907abc6996638935407f
Sam Lerner <samlerner@sams-mbp.attlocal.net>
2017-05-18 18:57:03 -0400

    dfkdkd

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -3,5 +3,5 @@ class Passage < ApplicationRecord
     validates :number, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     validates :text, presence: true
     validates :derivation, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-    validates :time_spent, numericality: { greater_than_or_equal_to: 0 }
+    validates :time_spent
 end
